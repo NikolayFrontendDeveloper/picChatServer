54713c10030b2d62916b062c9aadfbb5a4c711ec
Andriy Ivanchenko <clawhammer91@gmail.com>
2024-09-14 00:34:29 +0200

    feat: add socket receive_message

--- linstagramServer/src/server.js
+++ linstagramServer/src/server.js
@@ -37,6 +37,10 @@ io.on("connection", (socket) => {
         console.log(`User with ID: ${socket.id}} joined room: ${data}`)
     })
 
+    socket.on("send_message", (data) => {
+        socket.to(data.roomId).emit("receive_message", data);
+    })
+
     socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id);
     })
